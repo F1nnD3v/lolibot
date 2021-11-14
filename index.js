ab06edc4de26d29ed815c9c0f2b6bb5712590a95
F1nn <67049621+F1nnD3v@users.noreply.github.com>
2021-11-14 01:28:14 +0000

    welcome, bug fixes

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 //variáveis
-const { count, error, timeStamp } = require('console')
-const keepAlive = require("./server")
-const Discord = require('discord.js')
-const { maxHeaderSize } = require('http')
-const { listenerCount, disconnect } = require('process')
+const { count, error, timeStamp } = require('console');
+const keepAlive = require("./server");
+const Discord = require('discord.js');
+const { maxHeaderSize } = require('http');
+const { listenerCount, disconnect, config } = require('process');
 const ms = require('parse-ms');
-const { setTimeout } = require('timers')
-const { measureMemory } = require('vm')
-const client = new Discord.Client()
-var canalBemVindo = '760900791451123762'
-var starterRole = '760900731493810217'
-var canalLvlUp = '762280997626052618'
-const fs = require('fs')
-const { throws } = require('assert')
-const mongoose = require('mongoose')
-const prefix = process.env['prefix']
-
-var xpColdown = 3000
+const { setTimeout } = require('timers');
+const { measureMemory } = require('vm');
+const client = new Discord.Client();
+var canalBemVindo = '760900791451123762';
+var starterRole = '760900731493810217';
+var canalLvlUp = '762280997626052618';
+var canalRegras = '809475152894492693';
+var canalGeneral = '760900795075002459';
+var canalRoles = '762287996535832606';
 var canalComandos = '787407133775233035';
+const fs = require('fs');
+const { throws } = require('assert');
+const mongoose = require('mongoose');
+const prefix = process.env['prefix'];
+const idServer = '751050463079563364';
+const configs = require("./config.json");
+const helpComandoalias = require("./comandos/help.js");
+var giphy = require('giphy-api')('UkIViYzaHGIhWV9ufecEW91MELluDnB9');
+
+var xpColdown = 3000;
 //Embeds
 
 function mudarStats() {
-  var SttsAleatorio = ["My owner is developing me! ^-^", "If u need my help just type B!help in chat :)", "Im being developed yet, please dont think i ill do so much :*/", "Luke, i am your father", `If you type ${prefix}owner you can see all the social media of my creator!`, "Join in my server :3 --> https://discord.gg/hDdxcq7wwT"]
-  var ActivityTypeAleatorio = ["PLAYING", "STREAMING", "LISTENING", "WATCHING"]
-  var nAleatorioactivity = Math.floor(Math.random() * (ActivityTypeAleatorio.length - 1 - 0 + 1) + 0)
-  var nAleatorio = Math.floor(Math.random() * (SttsAleatorio.length - 1 - 0 + 1) + 0)
-  client.user.setActivity(SttsAleatorio[nAleatorio], { type: ActivityTypeAleatorio[nAleatorioactivity] })
-  console.log(ActivityTypeAleatorio[nAleatorioactivity], SttsAleatorio[nAleatorio])
+  var SttsAleatorio = ["My owner is developing me! ^-^", "If u need my help just type B!help in chat :)", "Im being developed yet, please dont think i ill do so much :*/", "Luke, i am your father", `If you type ${config.prefix || prefix}owner you can see all the social media of my creator!`, "Join in my server :3 --> https://discord.gg/hDdxcq7wwT"];
+  var ActivityTypeAleatorio = ["PLAYING", "STREAMING", "LISTENING", "WATCHING"];
+  var nAleatorioactivity = Math.floor(Math.random() * (ActivityTypeAleatorio.length - 1 - 0 + 1) + 0);
+  var nAleatorio = Math.floor(Math.random() * (SttsAleatorio.length - 1 - 0 + 1) + 0);
+  client.user.setActivity(SttsAleatorio[nAleatorio], { type: ActivityTypeAleatorio[nAleatorioactivity] });
+  console.log(ActivityTypeAleatorio[nAleatorioactivity], SttsAleatorio[nAleatorio]);
 }
 
+
+
 //Connecting to database
-mongoose.connect(process.env['dbPass'], {
+mongoose.connect(process.env['dbPass'] || configs.dbPass, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -72,65 +81,46 @@ client.on('ready', () => {
   channel.setName("Members: " + guild.memberCount)
   channelStaffsOn.setName("Staff's: " + guild.roles.cache.get('760900730164609065').members.size);
   channelBotsOn.setName("Bots: " + guild.roles.cache.get('760900732487598120').members.size);
-  setInterval(mudarStats, 300000);
-  guild.members.cache.forEach(p => {
-    console.log(client.users.cache.get(p.author.id).username + " está sendo analizando...")
-    Data.findOne({
-      userID: p.author.id,
-    }, (err, data) => {
-      if (err) console.log(err)
-      if (!data) {
-        const newData = new Data({
-          name: client.users.cache.get(p.author.id).username,
-          userID: p.author.id,
-          lb: "all",
-          coins: 0,
-          daily: 0,
-          xptime: 0,
-          warns: 0,
-          lvl: 0,
-          premium: false,
-          xp: 0,
-          hasChannelVC: false,
-          hasChanneltxt: false,
-          hasColor: false,
-          invites: 0,
-        })
-        newData.save().catch(err => console.log(err));
-        p.roles.add('760900731493810217').catch(e => {
-        console.log(e);
-      });
-      } else {
-        if(data.lvl >= 15){
-        p.roles.add('760900733959929956').catch(e => {
-        console.log(e);
-      });
-        }
-      }
-    });
-    console.log(client.users.cache.get(p.author.id).username + " foi analizado com sucesso!")
-  })
+  setInterval(mudarStats, 1800000);
 })
   client.on('guildMemberAdd', member => {
-    let guild = client.guilds.cache.get('751050463079563364')
-    let channel = guild.channels.cache.get('767503486870093825')
-    let channelStaffsOn = guild.channels.cache.get('767503489785004042')
-    let channelBotsOn = guild.channels.cache.get('767503492666884147')
+    let guild = client.guilds.cache.get('751050463079563364');
+    let channel = guild.channels.cache.get('767503486870093825');
+    let channelStaffsOn = guild.channels.cache.get('767503489785004042');
+    let channelBotsOn = guild.channels.cache.get('767503492666884147');
     if (!member.user.bot) {
-      member.guild.channels.cache.get(canalBemVindo).send('Welcome,  ' + member.user.username.toString() + "! I hope u enjoy!")
+      var gif;
+      giphy.random('anime').then(function (res) {
+        gif = res.data.embed_url;
+      });
+      let welcomeEmbed = new Discord.MessageEmbed()
+        .setTitle(`Welcome to ${client.guilds.cache.get(idServer).name}, ${member.user.username}`)
+        .addField(`I recommend you to read the ${member.guild.channels.cache.get(canalRegras)}`)
+        .addField(`You can also use the different channel commands, you can check all by typing ${configs.prefix}help`)
+        .addField(`Set your roles at ${member.guild.channels.cache.get(canalRoles)}`)
+        .addField(`And say hello in ${member.guild.channels.cache.get(canalGeneral)}`)
+        .setThumbnail(member.user.displayAvatarURL())
+        .setImage(gif)
+        .setColor('#0099ff')
+        .setTimestamp();
+      
+      member.guild.channels.cache.get(canalBemVindo).send(`Hi, ${member}`);
+      member.guild.channels.cache.get(canalBemVindo).send(welcomeEmbed);
       member.roles.add(starterRole).catch(console.error);
     }
-    channel.setName("Members: " + guild.memberCount);
-    channelStaffsOn.setName("Staff's: " + guild.roles.cache.get('760900730164609065').members.size);
-    channelBotsOn.setName("Bots: " + guild.roles.cache.get('760900732487598120').members.size);
+    channel.setName("Members: " + guild.memberCount)
+    channelStaffsOn.setName("Staff's: " + guild.roles.cache.get('760900730164609065').members.size)
+    channelBotsOn.setName("Bots: " + guild.roles.cache.get('760900732487598120').members.size)
+    if (guild.memberCount >= 100) {
+      message.guild.channels.cache.get(canalAnuncios).send("Hello everyone! Im here to say that the server has more than 100 members, and for that im here to give a <@&760900738409824276> to everyone for 5 days!!");
+    }
   })
   client.on('guildMemberRemove', member => {
     let guild = client.guilds.cache.get('751050463079563364')
     let channel = guild.channels.cache.get('767503486870093825')
     let channelStaffsOn = guild.channels.cache.get('767503489785004042')
     let channelBotsOn = guild.channels.cache.get('767503492666884147')
-    member.guild.channels.cache.get(canalBemVindo).send('Goodbye,  ' + member.user.username + "! We will miss u T-T!")
-    member.roles.add(starterRole).catch(console.error);
+    //member.guild.channels.cache.get(canalBemVindo).send('Goodbye,  ' + member.user.username + "! We will miss u T-T!")
     channel.setName("Members: " + guild.memberCount)
     channelStaffsOn.setName("Staff's: " + guild.roles.cache.get('760900730164609065').members.size)
     channelBotsOn.setName("Bots: " + guild.roles.cache.get('760900732487598120').members.size)
@@ -266,4 +256,4 @@ client.on('roleUpdate', member => {
 });
 
   keepAlive();
-  client.login(process.env['token']);
+  client.login(process.env['token'] || configs.token);
